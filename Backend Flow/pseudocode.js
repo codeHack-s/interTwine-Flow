0eb9463c0fff92026f7ebe0370787fca7dd9c8f0
Steve Tom <tomsteve187@gmail.com>
2023-05-06 23:15:27 +0300

    created the pseudocodes

---
+++ Backend Flow/pseudocode.js
@@ -0,0 +1,25 @@
+// how we handle data in the backend
+
+// after user clicks send message
+
+function sendMessage() {
+
+    // track where the message came from ie whatsapp or messaging
+
+    // send to that specific endpoint for the message
+
+    // store the data in the db
+
+    // wait for a response
+
+    // store the response
+
+}
+
+function listenForNewMessage() {
+
+    // wait for new messages
+
+}
+
+// update the ui with the latest message and push a notification
---
+++ User Flow/pseudocode.js
@@ -0,0 +1,49 @@
+// user navigated to the site
+
+if (isRegistered) {
+
+    login()
+    after_login()
+
+} else {
+
+    sign_up()
+}
+
+function login() {
+    // user enters login info
+    // user clicks login button
+    // user is logged in
+}
+
+function sign_up() {
+    // user enters sign up info
+    // user clicks sign up button
+    // user is registered and logged in
+}
+
+function after_login() {
+    showComponents()
+}
+
+function showComponents() {
+
+    // text box to enter message
+    // message area to show all messages
+    // button to reply
+    // sidebar 
+    sidebar()
+}
+
+function sidebar() {
+    // user can see chat categories
+    // user can see their app -- facebook,app, ig
+    // three sections -- apps messages and actual chats(conversations)
+}
+
+function authApps(appName, authToken, Headers) {
+
+    // authenticate user to an application
+
+}
+
